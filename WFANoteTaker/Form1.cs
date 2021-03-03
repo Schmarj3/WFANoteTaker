b302afbdd8c994d7b8f272a754ef79535d4d7089
Marj Etienne <marj.etienne@trupanion.com>
2021-03-02 19:30:09 -0800

    added button actions  in form1 and active control for titleText in form1 designer

--- WFANoteTaker/Form1.cs
+++ WFANoteTaker/Form1.cs
@@ -26,14 +26,42 @@ namespace WFANoteTaker
             table.Columns.Add("Message", typeof(String));
 
             dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Message"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 180;
         }
 
         private void buttonNew_Click(object sender, EventArgs e)
         {
-            labelTitle.Text = "";
-            labelMessage.Text = "";
+            titleText.Clear();
+            messageText.Clear();
+
         }
 
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(titleText.Text, messageText.Text);
 
+            titleText.Clear();
+            messageText.Clear();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if (index > -1)
+            {
+                titleText.Text = table.Rows[index].ItemArray[0].ToString();
+                messageText.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
     }
 }
